dac922b8068c9f7e85b94deb9244e3189b05c188
anwsimon <anna.w.simon1@gmail.com>
2021-09-20 18:44:32 -0400

    end of day

---
+++ server/db/models/Cart.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Cart = db.define("cart", {
+  products: {
+    type: Sequelize.ARRAY,
+    defaultValue: [],
+    allowNull: false
+  },
+  totalPrice: {
+    type: Sequelize.FLOAT
+  },
+
+});
+
+module.exports = Cart;
