759779c2ed9486d0eb9bf1af8b4390f9b3d6cc1e
Gigi Markley <gigimarkley@Admins-MacBook-Pro.local>
2021-09-21 09:32:04 -0700

    working on users API route

--- server/api/users.js
+++ server/api/users.js
@@ -1,20 +1,64 @@
 const router = require("express").Router();
 const {
-  models: { User },
+  models: { User, Cart },
 } = require("../db");
 module.exports = router;
 
+//Get all the user info for the admin
 router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      // A - check if user is admin, and user info
-      attributes: ["id", "username"],
+      attributes: ["id", "username", "email"],
     });
     res.json(users);
   } catch (err) {
     next(err);
   }
 });
+
+//Get individual user account details
+router.get("/:userId", async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId, {
+      attributes: ["id", "username", "email"],
+    });
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Get individual user cart details
+router.get("/:userId/cart", async (req, res, next) => {
+  try {
+    const usersCart = await Cart.findAll({
+      where: { userId: req.params.userId },
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// //Update individual user cart total
+// router.put("/:userId/cart", async (req, res, next) => {
+//   try {
+//     const usersCart = await Cart.update({
+//       where: { userId: req.params.userId },
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+//Add/POST user
+//need to add something to allow an admin user
+//for now everyone created is a customer
+router.post("/", async (req, res, next) => {
+  try {
+    res.status(201).json(await User.create(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
+
+//eventually we want admin to delete/edit, user to edit/delete
