a6fd52b1c3174d6e0b779a94eb7b47158e5b1524
Gigi Markley <gigimarkley@Admins-MacBook-Pro.local>
2021-09-22 09:31:51 -0700

    Updating the users API route. I added delete user and new add user. Separated sections of routes that are order specific and just user specific

--- server/api/users.js
+++ server/api/users.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const {
-  models: { User, Cart, CartItem },
+  models: { User, Cart, CartItem, Product },
 } = require("../db");
-const Product = require("../db/models/Product");
+
 module.exports = router;
 
-//Get all the user info for the admin
+//need to update cart to order
+
+//////////THIS SECTION IS PURELY USERS ROUTES. NOT ORDER INFO.//////////////////////
+//Get all the users - only for admin access
 router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll({
@@ -17,7 +20,7 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-//Get individual user account details
+//Get individual user account details - only for matching user and admin access
 router.get("/:userId", async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.userId, {
@@ -29,71 +32,7 @@ router.get("/:userId", async (req, res, next) => {
   }
 });
 
-// User.findAll({
-//   where: {
-//     '$Instruments.size$': { [Op.ne]: 'small' }
-//   },
-//   include: [{
-//     model: Tool,
-//     as: 'Instruments'
-//   }]
-// });
-
-//Get individual user cart details
-router.get("/:userId/cart", async (req, res, next) => {
-  try {
-    const cart = await Cart.findOne({
-      where: {
-        userId: req.params.userId
-      }});
-
-      const items = await CartItem.findAll({
-        where: {
-          cartId: cart.id
-        }
-      })
-
-    console.log("THESE ARE OUR ITEMS", items)
-    res.json(items)
-  } catch (err) {
-    next(err);
-  }
-});
-
-// //Update individual user cart
-// router.put("/:userId/cart", async (req, res, next) => {
-//   try {
-//     const usersCart = await Cart.findOne({
-//       where: {
-//         userId: req.params.userId,
-
-//       },
-//     });
-//     res.json(await usersCart.update(req.body));
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-// //update quantity of cart item in the cart
-// router.put("/:userId/cart", async (req, res, next) => {
-//   try {
-//     const usersCart = await Cart.findOne({
-//       where: { userId: req.params.userId },
-//     });
-//     res.json(await usersCart.update(req.body));
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-//if cart item quanity is 0, delete item
-
-// router.delete();
-
-//Add/POST user
-//need to add something to allow an admin user
-//for now everyone created is a customer
+//Add/POST user - creating a new user
 router.post("/", async (req, res, next) => {
   try {
     res.status(201).json(await User.create(req.body));
@@ -102,4 +41,61 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-//eventually we want admin to delete/edit, user to edit/delete
+//Delete user - admin only
+router.delete("/:userId", async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId);
+    await user.destroy();
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
+});
+/////////////////////////////ABOVE SECTION IS PURELY USERS ROUTES. NOT ORDER INFO.///////////////////////////////////
+
+/////////////////////////SECTION BELOW IS STUFF FOR ORDERS/////////////////////////////////////
+//Get individual user order details  - only for matching user and admin access
+router.get("/:userId/cart", async (req, res, next) => {
+  try {
+    const cart = await Cart.findOne({
+      where: {
+        userId: req.params.userId,
+      },
+    });
+
+    const items = await CartItem.findAll({
+      where: {
+        cartId: cart.id,
+      },
+    });
+
+    res.json(items);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Update individual user order  - only for matching user and admin access
+router.put("/:userId/cart", async (req, res, next) => {
+  try {
+    const usersCart = await Cart.findOne({
+      where: {
+        userId: req.params.userId,
+      },
+    });
+    res.json(await usersCart.update(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
+
+//if cart item quanity is 0, delete item
+router.delete("/:userId/cart/:cartItemId", async (req, res, next) => {
+  try {
+    const cartItem = await CartItem.findByPk(req.params.cartItemId);
+    await cartItem.destroy();
+    res.send(cartItem);
+  } catch (err) {
+    next(err);
+  }
+});
