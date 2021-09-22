e246bf9d1e02b5f9f21f83226c613d469de3545e
anwsimon <anna.w.simon1@gmail.com>
2021-09-22 12:26:32 -0400

    update: to db models and seed file

--- server/db/index.js
+++ server/db/index.js
@@ -5,32 +5,24 @@ const db = require("./db");
 const User = require("./models/User");
 const Product = require("./models/Product");
 const Order = require("./models/Order");
-const Cart = require("./models/Cart");
 const CartItem = require("./models/CartItem");
 
 //user and order
 User.hasMany(Order);
 Order.belongsTo(User);
 
-//product and order
-Product.belongsToMany(Order, { through: "orderItem" });
-Order.belongsToMany(Product, { through: "orderItem" });
-
-//User and Shopping Session
-Cart.belongsTo(User);
-User.hasOne(Cart);
-
 //Product and Cart Item
-Product.belongsToMany(Cart, { through: CartItem });
-Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Order, { through: CartItem });
+Order.belongsToMany(Product, { through: CartItem });
 
 module.exports = {
   db,
   models: {
     User,
     Product,
-    Cart,
     Order,
     CartItem
   },
 };
+
+//updated associations, imports, exports
