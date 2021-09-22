6b813078419935eaf06619409d9c8837e1e36c32
Kristina Stefanelli <kristinastefanelli@gmail.com>
2021-09-22 12:53:03 -0400

    added all products, single products, add product, edit product and delete product routes

--- server/api/index.js
+++ server/api/index.js
@@ -6,7 +6,7 @@ router.use("/users", require("./users"));
 //ADDED API ROUTES
 // router.use("/cart", require("./cart"));
 router.use("/order", require("./order"));
-router.use("/product", require("./product"));
+router.use("/products", require("./product"));
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
